e943ca4b70ec5014a70feedea8d9813ca59aae47
Bart Mars <32029310+bartmars@users.noreply.github.com>
2023-07-17 18:37:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var projectsContent = document.getElementById("projectsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutContent");
+
+    member.style.backgroundColor = "#D3D3D3";
+    skills.style.backgroundColor = "#F5F5F5";
+    projects.style.backgroundColor = "#F5F5F5";
+    contact.style.backgroundColor = "#F5F5F5";
+    about.style.backgroundColor = "#F5F5F5";
+
+    memberContent.style.display = "block";
+    skillsContent.style.display = "none";
+    projectsContent.style.display = "none";
+    contactContent.style.display = "none";
+    aboutContent.style.display = "none";
+}
